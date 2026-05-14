2124004923bbf9a9a6b90d97c7e158f801e54ed1
jasmeetkaur9 <jasmeet7642@gmail.com>
2026-05-14 20:35:09 +0000

    Add Foward Pass

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ from transformer_heads import FeedForward, SingleHead
 import gymnasium as gym
 from tqdm import tqdm 
 import numpy as np 
+from dataset import DatasetGenerator
 torch.set_default_dtype(torch.float64)
 import os 
 import gc 
@@ -42,4 +43,30 @@ class DecisionTransformer(torch.nn.Module):
         self.ffn       = FeedForward(self.n_embed).to(self.device)
 
         self.optimizer = torch.optim.Adam(self.parameters(), lr = lr)
+
+        self.dg = DatasetGenerator(batch_size, self.horizon_lenght, dataset_id)
+        self.dg.setup_episodes(sampled_ep)
+
+    
+    def forward(self, states, actions, returns_to_go, horizon_length):
+
+        pos_em = self.pos_embedding(torch.arange(0, horizon_length, device=self.device))
+        state_em = self.state_embedding(states) + pos_em
+        action_em = self.action_embedding(actions) + pos_em 
+        reward_em = self.reward_embedding(returns_to_go) + pos_em 
+
+        batch_size, horizon_length, n_embed = state_em.shape
+        stack_input = torch.stack([state_em, action_em, reward_em], dim=2)
+        stack_input = stack_input.reshape(batch_size,3*horizon_length,n_embed)
+
+        output = self.attn_head(stack_input)
+        output = self.ffn(output)
+
+        out = output.reshape(batch_size, horizon_length, 3, self.n_embed).permute(0, 2, 1, 3)
+
+        state_preds = self.predict_state(out[:, 0])
+        action_preds = self.predict_action(out[:, 1])
+        return_preds = self.predict_return(out[:, 2])
+
+        return output, state_preds, action_preds, return_preds
 
