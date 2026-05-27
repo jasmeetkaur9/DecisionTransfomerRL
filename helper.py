22c582d8035cce8cab6f1aeabcb8392ff59986c7
maupertuisbug <researchj09@gmail.com>
2026-05-27 16:44:31 +0000

    Adjust ImagSize

---
+++ helper.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+target_size = (250, 250)  # width, height
+
+for i, path in enumerate(["imgs/ant.png", "imgs/hc.png", "imgs/hopper.png"]):
+    img = Image.open(path)
+    # Center crop
+    w, h = img.size
+    left = (w - target_size[0]) // 2
+    top = (h - target_size[1]) // 2
+    cropped = img.crop((left, top, left + target_size[0], top + target_size[1]))
+    cropped.save(f"{i+1}.png")
